22262366657f3c5f1f07f4304aaaa66b64627b71
danielcaldas <caldasjdaniel@gmail.com>
2019-10-14 15:42:03 +0800

    Dynamically generating test cases with run-fixture script

---
+++ fixtures/fixtures.spec.js
@@ -0,0 +1,15 @@
+
+    // WARNING: this file is generated automatically
+    const backgroundColorReplacer = require('../replacer');
+
+    describe('backgroundColorReplacer', () => {
+  
+        it("replace color-when-match", () => {
+          try {
+            const { target, options } = require(`./replace-color-when-match.js`);
+            expect(backgroundColorReplacer(target, options)).toMatchSnapshot();
+          } catch(error) {
+            expect(error).toMatchSnapshot();
+          }
+        });
+      });
--- run-fixtures.js
+++ run-fixtures.js
@@ -1,67 +1,61 @@
-// TODO: work to be done here includes navigate in the folder
-// __tests__/fixtures to read all fixtures and exec them
+const fs = require("fs");
+const FIXTURES_BASE_DIR = `${__dirname}/fixtures`;
 
-// below implementation in babel-plugin-cloudinary for guidance...
+/**
+ * This function generates a spec snippet for a given
+ * fixture contained within the provided files.
+ * @param {Array.<string>} files - files of the fixture to generate.
+ * @returns {undefined}
+ */
+function generateFixtureJestSnippets(files) {
+  if (!files || !files.length) {
+    throw new Error("A valid file name must be provided");
+  }
 
-// const fs = require("fs");
+  const open = `
+    // WARNING: this file is generated automatically
+    const backgroundColorReplacer = require('../replacer');
 
-// const FIXTURES_BASE_DIR = `${__dirname}/fixtures`;
+    describe('backgroundColorReplacer', () => {
+  `;
+  const close = `});`
+  const specs = files.map(fname => {
+      const input = fs.readFileSync(`${FIXTURES_BASE_DIR}/${fname}`);
+      const specName = fname.split('.js')[0].replace(/[-]/, ' ', 'gi');
 
-// /**
-//  * This function generates a spec snippet for a given
-//  * fixture contained within the provided directory name.
-//  * @param {string} dir - directory of the fixture to generate.
-//  * @returns {undefined}
-//  */
-// function generateFixtureSnippet(dir) {
-//   if (!dir) {
-//     throw new Error("A valid directory name must be provided");
-//   }
+      return `
+        it("${specName}", () => {
+          try {
+            const { target, options } = require(\`./${fname}\`);
+            expect(backgroundColorReplacer(target, options)).toMatchSnapshot();
+          } catch(error) {
+            expect(error).toMatchSnapshot();
+          }
+        });
+      `;
+  }).join('\n');
 
-//   let runtimeConfig;
-//   const input = fs.readFileSync(`${FIXTURES_BASE_DIR}/${dir}/input.js`);
+  return `${open}${specs}${close}`;
+}
 
-//   // load cloudinaryrc file, fallback to default if no local config to fixture is found
-//   try {
-//     runtimeConfig = require(`${FIXTURES_BASE_DIR}/${dir}/cloudinaryrc.json`);
-//   } catch (error) {
-//     runtimeConfig = require(`${FIXTURES_BASE_DIR}/../cloudinaryrc.json`);
-//   }
+/**
+ * This function parses a file tree structure
+ * and performs a snippet generation operation
+ * on the existent files.
+ * @param {Object} err - error object.
+ * @param {Array.<string>} files - list of file's names (the fixtures).
+ * @returns {undefined}
+ */
+function parseFileTree(err, files) {
+  if (err) {
+    // eslint-disable-next-line
+    console.error(err);
+    process.exit(1);
+  }
 
-//   const spec = `
-//     // WARNING: this file is generated automatically
-//     const babel = require("babel-core");
-//     const plugin = require("../../../lib");
-//     jest.mock("../../../cloudinaryrc.json", () => (${JSON.stringify(runtimeConfig)}), { virtual: true });
-//     it("${dir}", () => {
-//       try {
-//         const { code } = babel.transform(\`${input.toString().replace(/[`$]/g, "\\$&")}\`, { plugins: [plugin] });
-//         expect(code).toMatchSnapshot();
-//       } catch(error) {
-//         expect(error).toMatchSnapshot();
-//       }
-//     });
-//   `;
+  const tmp = generateFixtureJestSnippets(files.slice(1));
 
-//   fs.writeFileSync(`${FIXTURES_BASE_DIR}/${dir}/${dir}.spec.js`, spec);
-// }
+  fs.writeFileSync(`${FIXTURES_BASE_DIR}/fixtures.spec.js`, tmp);
+}
 
-// /**
-//  * This function parses a file tree structure
-//  * and performs a snippet generation operation
-//  * on the existent directories.
-//  * @param {Object} err - error object.
-//  * @param {Array.<string>} directories - list of directories names.
-//  * @returns {undefined}
-//  */
-// function parseFileTree(err, directories) {
-//   if (err) {
-//     // eslint-disable-next-line
-//     console.error(err);
-//     process.exit(1);
-//   }
-
-//   directories.map(generateFixtureSnippet);
-// }
-
-// fs.readdir(FIXTURES_BASE_DIR, parseFileTree);
+fs.readdir(FIXTURES_BASE_DIR, parseFileTree);
